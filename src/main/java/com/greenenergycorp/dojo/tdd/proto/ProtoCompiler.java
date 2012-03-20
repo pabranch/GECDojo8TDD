d2a6fc0c62952a036c6ea7820af5a132966e4011
Adam Bender <abender@greenenergycorp.com>
2012-03-20 09:22:39 -0600

    Cleaning up protos to remove white space and renaming to remove 'class' which is a java reserved word.

--- src/main/java/com/greenenergycorp/dojo/tdd/proto/ProtoCompiler.java
+++ src/main/java/com/greenenergycorp/dojo/tdd/proto/ProtoCompiler.java
@@ -3,5 +3,8 @@ package com.greenenergycorp.dojo.tdd.proto;
 
 public class ProtoCompiler
 {
-
+    public static void main( String[] args )
+    {
+        System.out.println( "A poor compiler implementation." );
+    }
 }
