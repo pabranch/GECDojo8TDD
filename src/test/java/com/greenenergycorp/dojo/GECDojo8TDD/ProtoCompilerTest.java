9c2165495203259784d2dde87535969247b143e2
Adam Bender <abender@greenenergycorp.com>
2012-03-20 09:24:26 -0600

    Added empty test to get test directory to be included.

---
+++ src/test/java/com/greenenergycorp/dojo/GECDojo8TDD/ProtoCompilerTest.java
@@ -0,0 +1,17 @@
+package com.greenenergycorp.dojo.GECDojo8TDD;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ProtoCompilerTest
+{
+
+    @Test
+    public void test()
+    {
+        fail( "Not yet implemented" );
+    }
+
+}
