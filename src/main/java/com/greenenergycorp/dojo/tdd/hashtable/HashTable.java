e2b64732263a5d2068084ceef7376591e34a73e0
Eric Keary <eric@ptarmigan>
2012-03-20 16:23:39 -0600

    initial hash table commit

--- src/main/java/com/greenenergycorp/dojo/tdd/hashtable/HashTable.java
+++ src/main/java/com/greenenergycorp/dojo/tdd/hashtable/HashTable.java
@@ -1,13 +1,9 @@
 package com.greenenergycorp.dojo.tdd.hashtable;
 
-/**
- * Hello world!
- *
- */
-public class HashTable 
+public class HashTable
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
---
+++ src/test/java/com/greenenergycorp/dojo/GECDojo8TDD/HashTableTest.java
@@ -0,0 +1,14 @@
+package com.greenenergycorp.dojo.GECDojo8TDD;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class HashTableTest
+{
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+}
