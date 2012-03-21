b4e0c2db7f0eeb84dbf576b8889f27249fdf1f40
Eric Keary <eric@ptarmigan>
2012-03-21 07:39:37 -0600

    Since I wanted to leave the default size at 16, I created another constructor that would take an array length and initialize the table with that.
    Calling that constructor with an array length of 0 causes the test to pass.

--- src/main/java/com/greenenergycorp/dojo/tdd/hashtable/HashTable.java
+++ src/main/java/com/greenenergycorp/dojo/tdd/hashtable/HashTable.java
@@ -2,8 +2,30 @@ package com.greenenergycorp.dojo.tdd.hashtable;
 
 public class HashTable
 {
+    private static final int defaultSize = 16;
+
+    private HashTableEntry[] table;
+
     public static void main(String[] args)
+    {}
+
+    public HashTable()
     {
-        System.out.println("Hello World!");
+        setTable(new HashTableEntry[defaultSize]);
+    }
+
+    public HashTable(int initialSize)
+    {
+        setTable(new HashTableEntry[initialSize]);
+    }
+
+    public HashTableEntry[] getTable()
+    {
+        return table;
+    }
+
+    public void setTable(HashTableEntry[] table)
+    {
+        this.table = table;
     }
 }
--- src/test/java/com/greenenergycorp/dojo/GECDojo8TDD/HashTableTest.java
+++ src/test/java/com/greenenergycorp/dojo/GECDojo8TDD/HashTableTest.java
@@ -1,14 +1,32 @@
 package com.greenenergycorp.dojo.GECDojo8TDD;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.greenenergycorp.dojo.tdd.hashtable.HashTable;
+import com.greenenergycorp.dojo.tdd.hashtable.HashTableEntry;
 
 public class HashTableTest
 {
-    @Test
-    public void test()
+    HashTable hashTableUnderTest;
+    HashTableEntry[] table;
+
+    @Before
+    public void setUp()
     {
-        fail("Not yet implemented");
+        hashTableUnderTest = new HashTable();
+        table = hashTableUnderTest.getTable();
+    }
+
+    @Test
+    public void testEmptyHashTable()
+    {
+        final int emptyTableSize = 0;
+        hashTableUnderTest = new HashTable(emptyTableSize);
+        table = hashTableUnderTest.getTable();
+
+        assertEquals("Empty hash table", emptyTableSize, table.length);
     }
 }
