777c5a2e761fa2951444551c318841c34652f94b
Eric Keary <eric@ptarmigan>
2012-03-21 07:34:14 -0600

    The first test is an empty hash table test. I needed to create the basic data structure to start with. The default size is 16, and size the array length is asserted, a test for an empty table of length 0 fails.
    Committing now before making the test pass.

---
+++ src/main/java/com/greenenergycorp/dojo/tdd/hashtable/HashTableEntry.java
@@ -0,0 +1,37 @@
+package com.greenenergycorp.dojo.tdd.hashtable;
+
+public class HashTableEntry
+{
+    String key;
+
+    int value;
+
+    public HashTableEntry()
+    {}
+
+    public HashTableEntry(String key, int value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    public void setKey(String key)
+    {
+        this.key = key;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+}
